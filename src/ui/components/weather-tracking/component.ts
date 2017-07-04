fde44240cb701e942ddc820e45d111af3e09186b
DonSalvadori <csalvi42@gmail.com>
2017-07-04 12:53:47 -0700

    removed api_key

--- src/ui/components/weather-tracking/component.ts
+++ src/ui/components/weather-tracking/component.ts
@@ -10,7 +10,7 @@ export default class Weather extends Component {
 
 	async loadWeather() {
 		let zip = this.args.zip || 97239;
-		let response = await fetch(`http://api.wunderground.com/api/625172310aff38a6/conditions/q/OR/Portland.json`
+		let response = await fetch(`http://api.wunderground.com/api/API_KEY/conditions/q/OR/Portland.json`
 );
 		this.weather = await response.json();
 		setTimeout( () => { this.loadWeather(); }, 2000);
