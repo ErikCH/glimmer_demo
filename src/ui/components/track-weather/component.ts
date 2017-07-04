85f5ee296b4d2501decca8fd86adeca8932030f9
DonSalvadori <csalvi42@gmail.com>
2017-07-04 13:41:45 -0700

    changed trackedweather component

--- src/ui/components/track-weather/component.ts
+++ src/ui/components/track-weather/component.ts
@@ -1,4 +1,18 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from "@glimmer/component";
 
 export default class TrackWeather extends Component {
+	@tracked weather;
+
+	constructor(options) {
+		super(options);
+		this.loadWeather();
+	}
+
+	async loadWeather() {
+		let zip = this.args.zip || 97239;
+		let response = await fetch(`http://api.wunderground.com/api/625172310aff38a6/conditions/q/OR/Portland.json`
+);
+		this.weather = await response.json();
+		setTimeout( () => { this.loadWeather(); }, 2000);
+	}
 };
