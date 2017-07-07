04fbb433913fabe2d3636e3a6e146f90e985e7b1
DonSalvadori <csalvi42@gmail.com>
2017-07-06 20:02:54 -0700

    displaying weather

--- src/ui/components/track-weather/component.ts
+++ src/ui/components/track-weather/component.ts
@@ -11,8 +11,7 @@ export default class TrackWeather extends Component {
 	async loadWeather() {
 		// let zip = this.args.zip || 97239;
 		let response = await fetch(`http://api.wunderground.com/api/625172310aff38a6/conditions/q/CA/San_Francisco.json`);
-		console.log(response);
 		this.weather = await response.json();
-		// setTimeout( () => { this.loadWeather(); }, 2000);
+		setTimeout( () => { this.loadWeather(); }, 3000);
 	}
 };
