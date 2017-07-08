810098232939726df2ae39ff2ea98aa534977a37
DonSalvadori <csalvi42@gmail.com>
2017-07-07 19:55:56 -0700

    added weather icon

--- src/ui/components/track-weather/component.ts
+++ src/ui/components/track-weather/component.ts
@@ -10,8 +10,15 @@ export default class TrackWeather extends Component {
 
 	async loadWeather() {
 		// let zip = this.args.zip || 97239;
-		let response = await fetch(`http://api.wunderground.com/api/625172310aff38a6/conditions/q/CA/San_Francisco.json`);
-		this.weather = await response.json();
-		setTimeout( () => { this.loadWeather(); }, 3000);
+		let response1 = await fetch(`http://api.wunderground.com/api/625172310aff38a6/geolookup/q/97239.json`);
+		this.area = await response1.json();
+		let city = this.area.location.city;
+		let state = this.area.location.state;
+
+		let response2 = await fetch(`http://api.wunderground.com/api/625172310aff38a6/conditions/q/${state}/${city}.json`);
+		this.weather = await response2.json();
+		console.log(this.weather.current_observation.temp_f);
+		console.log(this.weather.current_observation.temp_c);
+		setTimeout( () => { this.loadWeather(); }, 10000);
 	}
 };
