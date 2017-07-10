745d1802f3d08ce311ca33aa305d1d4f032e82f0
Erik Hanchett <erik.hanchett@gmail.com>
2017-07-09 22:31:07 -0700

    Added Zip Fix

--- src/ui/components/track-weather/component.ts
+++ src/ui/components/track-weather/component.ts
@@ -3,6 +3,7 @@ import Component, { tracked } from "@glimmer/component";
 export default class TrackWeather extends Component {
 	@tracked weather;
 	@tracked area;
+	addZip;
 
 	constructor(options) {
 		super(options);
@@ -13,17 +14,27 @@ export default class TrackWeather extends Component {
 		this.addZip = event.target.value;
 	}
 
+	clicked() {
+		console.log(this.addZip);
+		this.loadWeather();
+	}
+
 	async loadWeather() {
-		// let zip = this.args.zip || 97239;
-		let response1 = await fetch(`http://api.wunderground.com/api/625172310aff38a6/geolookup/q/97239.json`);
-		this.area = await response1.json();
+		let zip = this.addZip || 97239;
+		try {
+			let response1 = await fetch(`http://api.wunderground.com/api/625172310aff38a6/geolookup/q/${zip}.json`);
+			this.area = await response1.json();
+			console.log(this.area);
 
-		let city = this.area.location.city;
-		let state = this.area.location.state;
+			let city = this.area.location.city;
+			let state = this.area.location.state;
 
-		let response2 = await fetch(`http://api.wunderground.com/api/625172310aff38a6/conditions/q/${state}/${city}.json`);
-		this.weather = await response2.json();
-		console.log(this.weather.current_observation.temp_f);
-		setTimeout( () => { this.loadWeather(); }, 10000);
+			let response2 = await fetch(`http://api.wunderground.com/api/625172310aff38a6/conditions/q/${state}/${city}.json`);
+			this.weather = await response2.json();
+			console.log(this.weather.current_observation.temp_f);
+			setTimeout( () => { this.loadWeather(); }, 10000);
+		} catch (error) {
+			console.log('error');
+		}
 	}
 };
